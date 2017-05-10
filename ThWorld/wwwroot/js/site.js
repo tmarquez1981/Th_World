17d9321af2f6cb6c893c4a1fef89291d6eb71762
Tom <skatereal@hotmail.com>
2017-05-09 19:21:47 -0800

    Some more progress with the project, including jQuery

---
+++ ThWorld/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// site.js
+
+//immediately executed function
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Tom and Braelyn");
+
+    //var main = $("main");
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this); // represents the object that "this" is related to
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+    
+
+})();
